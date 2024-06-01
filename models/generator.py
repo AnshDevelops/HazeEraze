0ff1546b45ead2e1af21d77aacf549e650f58ccc
AnshDevelops <singhal.ansh0503@gmail.com>
2024-06-01 17:04:06 +0530

    improved readability

--- models/generator.py
+++ models/generator.py
@@ -1,64 +1,54 @@
-import torch
 import torch.nn as nn
+
+
+def conv_block(in_features, out_features, kernel_size, stride, padding, activation, use_dropout=False):
+    layers = [
+        nn.Conv2d(in_features, out_features, kernel_size, stride, padding, bias=False),
+        nn.InstanceNorm2d(out_features),
+        activation
+    ]
+    if use_dropout:
+        layers.append(nn.Dropout(0.5))
+    return nn.Sequential(*layers)
 
 
 class ResidualBlock(nn.Module):
     def __init__(self, in_features):
         super(ResidualBlock, self).__init__()
-        self.block = nn.Sequential(
-            nn.Conv2d(in_features, in_features, kernel_size=3, padding=1),
-            nn.BatchNorm2d(in_features),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_features, in_features, kernel_size=3, padding=1),
-            nn.BatchNorm2d(in_features)
+        self.conv_block = nn.Sequential(
+            conv_block(in_features=in_features, out_features=in_features, kernel_size=3, stride=1, padding=1,
+                       activation=nn.ReLU()),
+            conv_block(in_features=in_features, out_features=in_features, kernel_size=3, stride=1, padding=1,
+                       activation=nn.Identity(), use_dropout=True)
         )
 
     def forward(self, x):
-        return x + self.block(x)
+        return x + self.conv_block(x)
 
 
 class Generator(nn.Module):
-    def __init__(self, input_channels, output_channels, n_residual_blocks=9):
+    def __init__(self):
         super(Generator, self).__init__()
-
-        # Initial convolution block
-        model = [
-            nn.Conv2d(input_channels, 64, kernel_size=7, padding=3),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True)
-        ]
-
-        # Downsampling
-        in_features = 64
-        out_features = in_features * 2
-        for _ in range(2):
-            model += [
-                nn.Conv2d(in_features, out_features, kernel_size=3, stride=2, padding=1),
-                nn.BatchNorm2d(out_features),
-                nn.ReLU(inplace=True)
-            ]
-            in_features = out_features
-            out_features = in_features * 2
-
-        # Residual blocks
-        for _ in range(n_residual_blocks):
-            model += [ResidualBlock(in_features)]
-
-        # Upsampling
-        out_features = in_features // 2
-        for _ in range(2):
-            model += [
-                nn.ConvTranspose2d(in_features, out_features, kernel_size=3, stride=2, padding=1, output_padding=1),
-                nn.BatchNorm2d(out_features),
-                nn.ReLU(inplace=True)
-            ]
-            in_features = out_features
-            out_features = in_features // 2
-
-        # Output layer
-        model += [nn.Conv2d(in_features, output_channels, kernel_size=7, padding=3), nn.Tanh()]
-
-        self.model = nn.Sequential(*model)
+        self.initial = conv_block(in_features=3, out_features=64, kernel_size=7, stride=1, padding=3,
+                                  activation=nn.ReLU())
+        self.downsample = nn.Sequential(
+            conv_block(in_features=64, out_features=128, kernel_size=3, stride=2, padding=1, activation=nn.ReLU()),
+            conv_block(in_features=128, out_features=256, kernel_size=3, stride=2, padding=1, activation=nn.ReLU())
+        )
+        self.res_blocks = nn.Sequential(*[ResidualBlock(256) for _ in range(9)])
+        self.upsample = nn.Sequential(
+            conv_block(in_features=256, out_features=128, kernel_size=3, stride=1, padding=1, activation=nn.ReLU()),
+            nn.Upsample(scale_factor=2),
+            conv_block(in_features=128, out_features=64, kernel_size=3, stride=1, padding=1, activation=nn.ReLU()),
+            nn.Upsample(scale_factor=2)
+        )
+        self.final = nn.Conv2d(in_channels=64, out_channels=3, kernel_size=7, stride=1, padding=3)
+        self.tanh = nn.Tanh()
 
     def forward(self, x):
-        return self.model(x)
+        x = self.initial(x)
+        x = self.downsample(x)
+        x = self.res_blocks(x)
+        x = self.upsample(x)
+        x = self.final(x)
+        return self.tanh(x)
